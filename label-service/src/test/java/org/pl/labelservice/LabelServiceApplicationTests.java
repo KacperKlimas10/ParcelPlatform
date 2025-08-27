5245dd7134bdedea509aced6641cf2431e8b49c3
KacperKlimas10 <10kacper.klimas.lo1@gmail.com>
2025-08-27 02:25:29 +0200

    Provided infractructure via Docker Compose and developed initial version of storage-service

---
+++ label-service/src/test/java/org/pl/labelservice/LabelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pl.labelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ pricing-service/src/test/java/org/pl/pricingservice/PricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pl.pricingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
